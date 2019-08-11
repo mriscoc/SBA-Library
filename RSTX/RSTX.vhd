008bd904cd57d0000e4decfc7fc5e6535040d7da
Miguel A. Risco-Castillo <mriscoc@gmail.com>
2019-08-11 01:36:39 -0500

    Improve timming and stabilization

--- RSTX/RSTX.vhd
+++ RSTX/RSTX.vhd
@@ -3,8 +3,8 @@
 -- RSTX
 --
 -- Title: Serial Transmitter IPCore for SBA (Simple Bus Architecture)
--- Version: 0.8
--- Date: 2017/08/04
+-- Version: 0.9
+-- Date: 2019/08/04
 -- Author: Miguel A. Risco Castillo
 --
 -- sba webpage: http://sba.accesus.com
@@ -13,62 +13,9 @@
 -- Description: RS232 Serial transmitter IP Core, Flag TXready to read in bit 14
 -- of Data bus.
 --
--- Release Notes:
---
--- v0.8 2017/08/04
--- Change sysfrec to sysfreq
---
--- v0.7 2016/11/03
--- Added Snippet for RSTX
--- Added INT_O outport, this signal is active when RSTX is ready to send
--- Remove dependency of SBAPackage
---
--- v0.6 2016/06/09
--- Remove dependency of SBAconfig
--- Sysfrec is now a "generic" parameter
--- Follow SBA v1.1 guidelines
---
--- v0.5 
--- Make SBA buses generic std_logic_vectors
--- remove unused bits of DAT_O.
---
--- v0.4 2012/06/21
--- Timing improvements
---
--- v0.3
--- Adjust Baud Clock precision
---
--- v0.21
--- Minor error correct in SBAData process
---
--- v0.2
--- Initial Release
---
 --------------------------------------------------------------------------------
--- Copyright:
---
--- (c) 2008-2015 Miguel A. Risco Castillo
---
--- This code, modifications, derivate work or based upon, can not be used or
--- distributed without the complete credits on this header.
---
--- This version is released under the GNU/GLP license
--- http://www.gnu.org/licenses/gpl.html
--- if you use this component for your research please include the appropriate
--- credit of Author.
---
--- The code may not be included into ip collections and similar compilations
--- which are sold. If you want to distribute this code for money then contact me
--- first and ask for my permission.
---
--- These copyright notices in the source code may not be removed or modified.
--- If you modify and/or distribute the code to any third party then you must not
--- veil the original author. It must always be clearly identifiable.
---
--- Although it is not required it would be a nice move to recognize my work by
--- adding a citation to the application's and/or research.
---
--- FOR COMMERCIAL PURPOSES REQUEST THE APPROPRIATE LICENSE FROM THE AUTHOR.
+-- For Copyright and release notes please refer to:
+-- https://github.com/mriscoc/SBA-Library/tree/master/RSTX/readme.md
 --------------------------------------------------------------------------------
 
 Library IEEE;
@@ -97,52 +44,75 @@ end RSTX;
 
 architecture RSTX_arch1 of RSTX is
 
+type tstate  is (IdleSt, StartSt, DataSt, StopSt);   -- Tx Serial Communication States
+
+signal TxSt  : tstate;
 signal BDclk : std_logic;
 signal TXRDYi: std_logic;
 signal WENi  : std_logic;
-signal TxData: std_logic_vector(9 downto 0);
-signal TxC   : integer range 0 to 9;
-signal TxRun : std_logic;  
+signal TxREG : std_logic_vector(7 downto 0);
+signal TxShift:std_logic_vector(8 downto 0);
 
 constant BaudDV : integer := integer(real(sysfreq)/real(baud))-1;
 
 begin
 
-ShifData: process (RST_I,WENi,CLK_I)
+DebugGen: if debug=1 generate
+begin
+process(RST_I)
 begin
   if (RST_I='1') then
-    if (debug=1) then
-      Report "TX Baud: " &  integer'image(baud) & " real: " &integer'image(integer(real(sysfreq)/real((BaudDV+1))));
-    end if;
-    TxData <= (others=>'1');
+    Report "TX Baud: " &  integer'image(baud) & " real: " &integer'image(integer(real(sysfreq)/real((BaudDV+1))));
+  end if;
+end process;
+end generate;
+
+TxStProc:process(RST_I,CLK_I)
+variable cnt:integer range 0 to 7;
+begin
+  if (RST_I='1') then
+    TxSt<=IdleSt;
+    cnt:=0;
   elsif rising_edge(CLK_I) then
-    if WENi='1' then
-      TxData <= DAT_I(7 downto 0) & '0' & '1';   -- Datos & bit de inicio & idle
-    elsif BDclk='1' and (TxRun='1') then
-      TxData <= '1' & TxData(9 downto 1);
+    if (BDclk='1') then
+      case TxSt is
+        When IdleSt  => if WENi='1' then
+                          TxSt<=StartSt;
+                        end if;
+        When StartSt => TxSt<=DataSt;
+                        cnt:=0;
+        When DataSt  => if cnt>6 then
+                          TxSt<=StopSt;
+                        else
+                          cnt:=cnt+1;
+                        end if;
+        When StopSt  => TxSt<=IdleSt;
+        When others  => TxSt<=IdleSt;
+      end case;
+    end if;
+  end if;
+end process TxStProc;
+
+
+ShifData: process (CLK_I)
+begin
+  if rising_edge(CLK_I) then
+    if (BDclk='1') then
+      case TxSt is
+        When IdleSt  => TxShift(0) <= '1';        -- Idle
+        When StartSt => TxShift <= TxREG & '0';   -- Datos & bit de inicio
+        When DataSt  => TxShift <= '1' & TxShift(8 downto 1);  -- Shift
+        When StopSt  => TxShift(0) <= '1';        -- Stop bit
+        When others  => TxShift(0) <= '1';
+      end case;
     end if;
   end if;
 end process ShifData;
 
-TxProc:process (RST_I,CLK_I,WENi,BDclk)
-begin
-  if (RST_I='1') then
-    TxC   <= 0;
-    TxRun <='0';
-  elsif rising_edge(CLK_I) then  
-    if (WENi='1') then
-      TxC<=0;
-      TxRun<='1';
-    elsif BDclk='1' then
-      if (TxC=9) then TxRun<='0'; else TxC<=TxC+1; end if;
-    end if;
-  end if;
-end process TxProc;
-
-BaudGen: process (RST_I,CLK_I,WENi)
+BaudGen: process (RST_I,CLK_I)
 Variable cnt: integer range 0 to BaudDV;
 begin
-  if (RST_I='1') or (WENi='1') then
+  if (RST_I='1') then
     BDclk <= '0';
     cnt:=0;
   elsif rising_edge(CLK_I) then
@@ -156,9 +126,24 @@ begin
   end if;
 end process BaudGen;
 
-TX     <= TxData(0);
-WENi   <= (STB_I and WE_I and not RST_I);
-TXRDYi <= not (WENi or TxRun);
+SBAProc:process(RST_I,CLK_I)
+begin
+  if (RST_I='1') then
+    TxREG<=(others=>'0');
+    WENi <= '0';
+  elsif rising_edge(CLK_I) then
+    if (TxSt=StartSt) then
+      WENi <= '0';
+    end if;
+    if (STB_I='1' and WE_I='1') then
+      TxREG<=DAT_I(7 downto 0);
+      WENi <= '1';
+    end if;
+  end if;
+end process SBAProc;
+
+TX     <= TxShift(0);
+TXRDYi <= '1' when (TxSt=IdleSt) and (WENi='0') else '0';
 INT_O  <= TXRDYi;
 DAT_O(14)<=TXRDYi;
 
--- RSTX/readme.md
+++ RSTX/readme.md
@@ -1,51 +1,23 @@
-# **RSTX: Serial Transmitter IPCore for SBA**
-- - - 
-![](image.png)   
+RSTX
+====
+![](image.png)  
 
-**Version:** 0.7
+RSTX: Serial Transmitter IPCore for SBA  
+---------------------------------------
 
-**Date:** 2016/11/03
+**Version:** 0.9  
+**Date:** 2019/08/04  
+**Author:** Miguel A. Risco-Castillo  
+**Repository URL:** <https://github.com/mriscoc/SBA_Library/tree/master/RSTX>  
 
-**Author:** Miguel A. Risco-Castillo
-
-**CodeURL:** https://github.com/mriscoc/SBA_Library/blob/master/RSTX/RSTX.vhd  
-
-**Description:**
-RS232 Serial transmitter IP Core, Flag TXready to read in bit 14 of Data bus.
-
-**Release Notes**
-
-v0.7 2016/11/03
-- Added Snippet for RSTX
-
-v0.6 2016/06/09
-- Remove dependency of SBAconfig
-- Sysfrec is now a "generic" parameter
-- Follow SBA v1.1 guidelines
-
-v0.5
-- Make SBA buses generic std_logic_vectors
-- remove unused bits of DAT_O.
-
-v0.4 20120621
-- Timing improvements
-
-v0.3
-- Adjust Baud Clock precision
-
-v0.21
-- Minor error correct in SBAData process
-
-v0.2
-- Initial Release
-
-
+Interface of the VHDL module
+----------------------------
 
 ```vhdl
 entity RSTX is
 generic (
   debug:positive:=1;
-  sysfrec:positive:=50E6;
+  sysfreq:positive:=50E6;
   baud:positive:=57600
   );
 port (
@@ -56,8 +28,75 @@ port (
   WE_I  : in std_logic;
   DAT_I : in std_logic_vector;
   DAT_O : out std_logic_vector;
+  INT_O : out std_logic;
   -- UART Interface;
-  TX     :out std_logic
-  );
-end RSTX;
+  TX    : out std_logic
+   );
+end RSTX; 
 ```
+
+**Description:**
+RS232 Serial transmitter IP Core, Flag TXready to read in bit 14 of Data bus.
+
+
+Release Notes:
+--------------
+
+- v0.9 2019/08/04  
+  Improve Timming  
+
+- v0.8 2017/08/04  
+  Change sysfrec to sysfreq  
+
+- v0.7 2016/11/03  
+  Added Snippet for RSTX  
+  Added INT_O outport, this signal is active when RSTX is ready to send  
+  Remove dependency of SBAPackage  
+
+- v0.6 2016/06/09  
+  Remove dependency of SBAconfig  
+  Sysfrec is now a "generic" parameter  
+  Follow SBA v1.1 guidelines  
+
+- v0.5  
+  Make SBA buses generic std_logic_vectors  
+  remove unused bits of DAT_O.  
+
+- v0.4 2012/06/21  
+  Timing and code improvements  
+
+- v0.3  
+  Adjust Baud Clock precision  
+
+- v0.21 
+  Minor error correct in SBAData process  
+
+- v0.2 
+  Initial Release  
+
+--------------------------------------------------------------------------------
+ **Copyright:**  
+
+ (c) 2008-2015 Miguel A. Risco Castillo
+
+ This code, modifications, derivate work or based upon, can not be used or
+ distributed without the complete credits on this header.
+
+ This version is released under the GNU/GLP license
+ http://www.gnu.org/licenses/gpl.html
+ if you use this component for your research please include the appropriate
+ credit of Author.
+
+ The code may not be included into ip collections and similar compilations
+ which are sold. If you want to distribute this code for money then contact me
+ first and ask for my permission.
+
+ These copyright notices in the source code may not be removed or modified.
+ If you modify and/or distribute the code to any third party then you must not
+ veil the original author. It must always be clearly identifiable.
+
+ Although it is not required it would be a nice move to recognize my work by
+ adding a citation to the application's and/or research.
+
+ FOR COMMERCIAL PURPOSES REQUEST THE APPROPRIATE LICENSE FROM THE AUTHOR.
+--------------------------------------------------------------------------------
